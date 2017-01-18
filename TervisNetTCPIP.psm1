636327a2d08faa72122a3fc73349583fa25f33ac
Chris Magnuson <hourglasssand@gmail.com>
2017-01-18 15:24:34 -0500

    Functions moved from TervisTechnicalServicesLinux

---
+++ TervisNetTCPIP.psm1
@@ -0,0 +1,25 @@
+﻿Function Wait-ForPortAvailable {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)][Alias("IPAddress")]$ComputerName,
+        [Parameter(Mandatory)]$PortNumbertoMonitor
+    )
+    do {
+        Write-Verbose "Waiting for $ComputerName to come online..."
+        sleep 3
+    } until (Test-NetConnection -ComputerName $ComputerName -Port $PortNumbertoMonitor | Where { $_.TcpTestSucceeded })
+
+}
+
+Function Wait-ForPortNotAvailable {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)][Alias("IPAddress")]$ComputerName,
+        [Parameter(Mandatory)]$PortNumbertoMonitor
+    )
+
+    do {
+        Write-Verbose "Waiting for $ComputerName to shutdown..."
+        sleep 3
+    } While (Test-NetConnection -ComputerName $ComputerName -Port $PortNumbertoMonitor | Where { $_.TcpTestSucceeded })
+}
